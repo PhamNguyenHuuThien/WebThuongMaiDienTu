738ae0565408bc48c83194cd0f5844c56684834b
Thien <bibi9768@gmail.com>
2018-12-22 17:25:24 +0700

    Su dung Template

--- Client/AppThuongMaiDienTu/src/app/app.component.ts
+++ Client/AppThuongMaiDienTu/src/app/app.component.ts
@@ -1,26 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
+import { Platform, Nav } from "ionic-angular";
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { Keyboard } from '@ionic-native/keyboard';
+
+import { HomePage } from "../pages/home/home";
+import { LoginPage } from "../pages/login/login";
+import { LocalWeatherPage } from "../pages/local-weather/local-weather";
+import {SanPham } from "../pages/sanpham/sanpham";
+
+
+export interface MenuItem {
+    title: string;
+    component: any;
+    icon: string;
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html'
+  templateUrl: 'app.html'
 })
-export class AppComponent {
+
+export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+
+  rootPage: any = LoginPage;
+
+  appMenuItems: Array<MenuItem>;
+
   constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    public platform: Platform,
+    public statusBar: StatusBar,
+    public splashScreen: SplashScreen,
+    public keyboard: Keyboard
   ) {
     this.initializeApp();
+
+    this.appMenuItems = [
+      {title: 'Trang chủ', component: HomePage, icon: 'home'},
+      {title: 'Sản phẩm', component: SanPham, icon: 'flower'},
+      {title: 'Thời tiết', component: LocalWeatherPage, icon: 'partly-sunny'}
+
+    ];
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+
+      //*** Control Splash Screen
+      // this.splashScreen.show();
+      // this.splashScreen.hide();
+
+      //*** Control Status Bar
       this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      this.statusBar.overlaysWebView(false);
+
+      //*** Control Keyboard
+      // this.keyboard.disableScroll(true);
     });
   }
+
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
+  }
+
+  logout() {
+    this.nav.setRoot(LoginPage);
+  }
+
 }
--- Client/AppThuongMaiDienTu/src/app/app.module.ts
+++ Client/AppThuongMaiDienTu/src/app/app.module.ts
@@ -1,23 +1,89 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import {NgModule} from "@angular/core";
+import {IonicApp, IonicModule} from "ionic-angular";
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import {IonicStorageModule} from '@ionic/storage';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {Keyboard} from '@ionic-native/keyboard';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import {ActivityService} from "../services/activity-service";
+import {TripService} from "../services/trip-service";
+import {WeatherProvider} from "../services/weather";
+
+import {MyApp} from "./app.component";
+
+import {SettingsPage} from "../pages/settings/settings";
+import {CheckoutTripPage} from "../pages/checkout-trip/checkout-trip";
+import {HomePage} from "../pages/home/home";
+import {LoginPage} from "../pages/login/login";
+import {NotificationsPage} from "../pages/notifications/notifications";
+import {RegisterPage} from "../pages/register/register";
+import {SearchLocationPage} from "../pages/search-location/search-location";
+import {TripDetailPage} from "../pages/trip-detail/trip-detail";
+import {TripsPage} from "../pages/trips/trips";
+import {LocalWeatherPage} from "../pages/local-weather/local-weather";
+import {SanPham} from "../pages/sanpham/sanpham";
+
+// import services
+// end import services
+// end import services
+
+// import pages
+// end import pages
 
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  declarations: [
+    MyApp,
+    SettingsPage,
+    CheckoutTripPage,
+    HomePage,
+    LoginPage,
+    LocalWeatherPage,
+    NotificationsPage,
+    RegisterPage,
+    SearchLocationPage,
+    TripDetailPage,
+    TripsPage,
+    SanPham
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    IonicModule.forRoot(MyApp, {
+      scrollPadding: false,
+      scrollAssist: true,
+      autoFocusAssist: false
+    }),
+    IonicStorageModule.forRoot({
+      name: '__ionic3_start_theme',
+        driverOrder: ['indexeddb', 'sqlite', 'websql']
+    })
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    SettingsPage,
+    CheckoutTripPage,
+    HomePage,
+    LoginPage,
+    LocalWeatherPage,
+    NotificationsPage,
+    RegisterPage,
+    SearchLocationPage,
+    TripDetailPage,
+    TripsPage
+  ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
-  ],
-  bootstrap: [AppComponent]
+    Keyboard,
+    ActivityService,
+    TripService,
+    WeatherProvider
+  ]
 })
-export class AppModule {}
+
+export class AppModule {
+}
