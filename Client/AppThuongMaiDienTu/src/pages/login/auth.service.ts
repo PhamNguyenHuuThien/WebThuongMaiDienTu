f9d684d3707b014f45b491cf049520e6af6b0984
Thien <bibi9768@gmail.com>
2018-12-31 11:15:17 +0700

    Sua trang DangNhap

---
+++ Client/AppThuongMaiDienTu/src/pages/login/auth.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import * as moment from 'moment';
+declare var ApiUrl: any;
+
+export class AuthService {
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  // Gọi API login vào hệ thống
+  login(tendangnhap: string, matkhau: string) {
+    return this.http.post<any>(ApiUrl + '/dangnhap.php', { tendangnhap, matkhau });
+  }
+
+  // Lưu JWT ra localStorage
+  setSession(authResult) {
+    localStorage.setItem('id_token', authResult.token);
+  }
+
+  // Khi logout thì xóa token ra khỏi storage
+  logout() {
+    localStorage.removeItem('id_token');
+  }
+
+  // Kiểm tra đã đăng nhập chưa, dựa vào expire date
+  public isLoggedIn() {
+    return moment().isBefore(this.getExpiration());
+  }
+
+  // lấy expire date của JWT
+  getExpiration() {
+    const token = this.decodeToken(localStorage.getItem('id_token'));
+    if (token === null) {
+      return moment().add(-10000, 'd').toDate();
+    }
+
+    return moment.unix(token.exp).toDate();
+  }
+
+  private decodeToken(token: string) {
+    if (!token) {
+      return null;
+    }
+
+    try {
+      const base64Url = token.split('.')[1];
+      const base64 = base64Url.replace('-', '+').replace('_', '/');
+      return JSON.parse(window.atob(base64));
+    } catch (e) {
+      return null;
+    }
+  }
+
+}
--- Client/AppThuongMaiDienTu/src/pages/login/login.ts
+++ Client/AppThuongMaiDienTu/src/pages/login/login.ts
@@ -2,15 +2,20 @@ import {Component} from "@angular/core";
 import {NavController, AlertController, ToastController, MenuController} from "ionic-angular";
 import {HomePage} from "../home/home";
 import {RegisterPage} from "../register/register";
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import {AuthService} from './auth.service';
+
 
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
 })
 export class LoginPage {
-
-  constructor(public nav: NavController, public forgotCtrl: AlertController, public menu: MenuController, public toastCtrl: ToastController) {
-    this.menu.swipeEnable(false);
+  public username;
+  public password;
+  //public response;
+  constructor( public http: HttpClient,public nav: NavController, public forgotCtrl: AlertController, public menu: MenuController, public toastCtrl: ToastController) {
+ this.menu.swipeEnable(false);
   }
 
   // go to register page
@@ -19,9 +24,59 @@ export class LoginPage {
   }
 
   // login and go to home page
-  login() {
+  login(username:string,password:string) {
+    console.log(username);
+    console.log(password);
+    console.log("Đăng nhập");
+  
+    // let data = {
+
+      // tendangnhap: this.username,
+      
+    // matkhau: this.password
+      
+    //   };
+      
+
+    
+
+  // Gọi phương thức login của AuthService class mà chúng ta đã viết ở phía trên
+  // AuthService.login(username,password).subscribe(
+  //     response => {
+  //       if (response.sucess) {
+
+  //         // Đăng nhập thành công thì lưu lại JWT vào storage, sau đó redirect tới trang dahshboard
+  //         this.authService.setSession(response);
+  //         this.router.navigateByUrl('/dashboard');
+  //       } else {
+  //         this.formErrorMessage = response.message;
+  //       }
+  //     },
+  //     error => {
+  //       this.formErrorMessage = 'Lỗi kết nối tới máy chủ';
+  //     }
+  //   );
+  
+  
+      
+      // console.log(view2)
+      // if(view2=="Your Login success")
+      // {
+        let toast = this.toastCtrl.create({
+          showCloseButton: true,
+          message: 'Đăng nhập thành công!',
+          duration: 3000,
+          position: 'bottom'
+        });
+        toast.onDidDismiss(() => {
+          console.log('Dismissed toast');
+        });
+        toast.present();
+      // }
     this.nav.setRoot(HomePage);
-  }
+  
+   }
+  
 
   forgotPass() {
     let forgot = this.forgotCtrl.create({
